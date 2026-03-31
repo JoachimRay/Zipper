ee37e7043b19d810d2ed737f06c8a5681155b4d9
JoachimRay <131645680+JoachimRay@users.noreply.github.com>
2026-03-31 10:55:05 +0800

    Small UI Changes

--- main/src/main/java/zipper/App.java
+++ main/src/main/java/zipper/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("UI"), 640, 480);
+        scene = new Scene(loadFXML("UI"), 640, 700);
         scene.getStylesheets().add(getClass().getResource("/zipper/UI.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
